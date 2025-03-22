61c5dfd5bdf622926aadebdceb40607168f6b268
JoseCarlosOlivaMedac <jor0011@alu.medac.es>
2025-03-22 09:21:29 +0100

    Puesta a punto del enrutador !!

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
-
+//Este es el Router que nos permitira movernos entre las vistas de la aplicacion , ademas se ha añadido una funcion de proteccion de rutas para que no se pueda acceder a la vista de carrito sin antes haber iniciado sesion
+//Se importa el router de vue y se crean las rutas de la aplicacion
+//Se importan las vistas de Productos, Login y Carrito
 import { createRouter, createWebHistory } from 'vue-router';
 import Productos from '../views/Productos.vue';
 import Login from '../views/Login.vue'; 
